e915ac0cb904e601d62d56062d4837cc21550c72
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-06-22 12:09:38 +0600

    react-redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import store from './store/store';
 import App from './App';
 import './index.css';
 
@@ -9,7 +10,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
