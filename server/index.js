3aa63ed586e825ea96af39578be95df6f1f81505
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-06-22 11:31:45 +0600

    backend-server-setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+
+app.use(cors());
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello Server is started');
+});
+
+io.on('connection', (socket) => {
+    console.log(`user connected of the id : ${socket.id}`)
+});
+
+const PORT = process.env.PORT || 3003;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
