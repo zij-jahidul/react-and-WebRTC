706ff68ee28e2a9bb36d4080c843286e31c25ef9
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-06-22 12:46:53 +0600

    code refactoring

--- frontend/src/LoginPage/LoginPage.js
+++ frontend/src/LoginPage/LoginPage.js
@@ -1,26 +1,28 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
+import Logo from './Logo';
+import LoginInput from './LoginInput';
+import LoginButton from './LoginButton';
 
-const Logo = () => {
-    return <p className='logo'>GeoCall</p>
-}
-
-const LoginInput = () => {
-    return <input className='l_page_input' />
-}
-
-const LoginButton = () => {
-    return <button className='l_page_login_button'>Login</button>
+const isUsernameValid = (username) => {
+    return username.length > 0 && username.length < 10 && !username.includes(' ');
 }
 
 function LoginPage() {
+    const [username, setUsername] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate('/map');
+    }
+
     return (
         <div className='l_page_main_container'>
             <div className='l_page_box'>
                 <Logo />
-                <LoginInput />
-                <LoginButton />
+                <LoginInput username={username} setUsername={setUsername} />
+                <LoginButton disabled={!isUsernameValid(username)} onClickHandler={handleLogin} />
             </div>
         </div>
     )
---
+++ frontend/src/LoginPage/Logo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Logo = () => {
+    return <p className='logo'>GeoCall</p>
+}
+
+export default Logo;
+
