cb0bba04358effcc78cdd88aa586123e121c74a6
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-06-23 11:11:57 +0600

    google-map-react setup

--- frontend/src/MapPage/MapPage.js
+++ frontend/src/MapPage/MapPage.js
@@ -1,7 +1,33 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { useSelector } from "react-redux";
+
+import "./MapPage.css";
 
 const MapPage = () => {
-    return <div>Map page</div>;
+    const myLocation = useSelector((state) => state.map.myLocation);
+    // const onlineUsers = useSelector((state) => state.map.onlineUsers);
+    // const cardChosenOption = useSelector((state) => state.map.cardChosenOption);
+
+    const defaultMapProps = {
+        center: {
+            lat: myLocation.lat,
+            lng: myLocation.lng,
+        },
+        zoom: 11,
+    };
+
+    return (
+        <div className="map_page_container">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: '' }}
+                defaultCenter={defaultMapProps.center}
+                defaultZoom={defaultMapProps.zoom}
+            >
+
+            </GoogleMapReact>
+        </div>
+    );
 };
 
 export default MapPage;
